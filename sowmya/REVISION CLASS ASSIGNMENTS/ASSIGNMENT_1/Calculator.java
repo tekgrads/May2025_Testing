560e125935d14dc4335d18fc394ca44a6d540b86
9701938309 <gandhamsowmyag@gmail.com>
2025-06-23 11:17:22 +0530

    changes
    changes

---
+++ sowmya/REVISION CLASS ASSIGNMENTS/ASSIGNMENT_1/Calculator.java
@@ -0,0 +1,36 @@
+// Calculator.java
+public class Calculator {
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b !=0)
+            return (double) a / b;
+        else {
+            System.out.println("Error: Division by zero.");
+            return 0;
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("Addition: " + add(num1, num2));
+        System.out.println("Subtraction: " + subtract(num1, num2));
+        System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Division: " + divide(num1, num2));
+    }
+}
