75bfefa87da565ad8b1c506a311073a875259cc7
Anusha <anushabomme618@gmail.com>
2025-07-03 15:58:42 +0530

    changes

---
+++ anusha/Java Assignments/Assignments On Wrapper Classes and Exception handling/Assignment 2/InvalidMarkException.java
@@ -0,0 +1,6 @@
+// Custom exception class to handle invalid marks
+public class InvalidMarkException extends Exception {
+    public InvalidMarkException(String message) {
+        super(message);
+    }
+}
